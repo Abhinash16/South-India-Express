a2b25fff4536afb96db277105fbbf4aea415d008
saikittu332 <saikittu332@github.com>
2019-09-19 13:20:24 +0530

    contact done

---
+++ frontend/south-india-front/src/components/contact.vue
@@ -0,0 +1,95 @@
+ <template>
+<div class="mbr-section form4 cid-qv5Aq4h3k3" id="form4-2y" data-rv-view="5142"> 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                
+                <div class="google-map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3223.009800035491!2d77.61026392869192!3d12.934953630476459!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae144d92b2dd49%3A0x90042049104d5e79!2sForum%20Mall!5e0!3m2!1sen!2sus!4v1568875757698!5m2!1sen!2sus" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div>
+                    <div class="icon-block pb-3">
+                        <span class="icon-block__icon">
+                            <span class="mbri-letter mbr-iconfont" media-simple="true"></span>
+                        </span>
+                        <h4 class="icon-block__title align-left mbr-fonts-style display-5">
+                            Don't hesitate to contact us
+                        </h4>
+                    </div>
+                    <div class="icon-contacts pb-3">
+                        
+                        <p class="mbr-text align-left mbr-fonts-style display-7">
+                            Phone: +91 9876543210 <br>
+                            Email: contactus@southindianexpress.com
+                        </p>
+                    </div>
+                </div>
+                <div data-form-type="formoid">
+                    <div data-form-alert="" hidden="">
+                        Thanks for filling out the form!
+                    </div>
+                    <form class="block mbr-form" action="" method="post" data-form-title="Contact Form"><input type="hidden" data-form-email="true" value="44AuoOeYtJwJhCQAt1bwP6le5Z4RfRd5BZf43WsjSuwHcT0Qs78ZrHvX5++bPVZSjP2KWAwu7FMdyPJSg+JHFd9WHR94bYwVjv67MUE8EdCzbC0NmF20TCEyJMj7IT0z">
+                        <div class="row">
+                            <div class="col-md-6 multi-horizontal" data-for="name">
+                                <input type="text" class="form-control input" name="name" data-form-field="Name" placeholder="Your Name" required="" id="name-form4-2y">
+                            </div>
+                            <div class="col-md-6 multi-horizontal" data-for="phone">
+                                <input type="text" class="form-control input" name="phone"  data-form-field="Phone" placeholder="Phone" required="" id="phone-form4-2y">
+                            </div>
+                            <div class="col-md-12" data-for="email">
+                                <input type="text" class="form-control input" name="email" data-form-field="Email" placeholder="Email" required="" id="email-form4-2y">
+                            </div>
+                            <div class="col-md-12" data-for="message">
+                                <textarea class="form-control input" name="message" rows="3" data-form-field="Message" placeholder="Message" style="resize:none" id="message-form4-2y"></textarea>
+                            </div>
+                            <div class="input-group-btn col-md-12" style="margin-top: 10px;">
+                                <button href="" type="submit" class="btn btn-primary btn-form display-4">SEND MESSAGE</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>   
+
+</template>   
+
+
+<style scoped>
+
+
+.cid-qv5Aq4h3k3 .input {
+    margin-bottom: 15px;
+}
+
+.cid-qv5Aq4h3k3 .icon-block .icon-block__title {
+    display: inline-block;
+    align-self: center;
+    -webkit-align-self: center;
+    margin-bottom: 0;
+    line-height: 1;
+    font-style: italic;
+}
+
+.form-control {
+    background-color: #f5f5f5;
+    box-shadow: none;
+    color: #565656;
+    font-family: 'Rubik', sans-serif;
+    font-size: 1rem;
+    line-height: 1.43;
+    min-height: 3.5em;
+    padding: 1.07em .5em;
+    
+}
+
+@media (min-width: 576px){
+.container {
+    margin-bottom: 15px;
+}
+}
+
+</style>
--- frontend/south-india-front/src/routes.js
+++ frontend/south-india-front/src/routes.js
@@ -2,12 +2,14 @@ import HomeComponent from './components/home'
 import packages from './components/packages.vue';
 import login from './components/login'
 import signup from './components/signup';
+import contact from './components/contact';
 
 export default[
     {path:'/', component:HomeComponent},
     {path:'/packages', component:packages},
     {path:'/login', component:login},
     {path:'/signup', component:signup},
+    {path:'/contact', component:contact},
     
 
 
