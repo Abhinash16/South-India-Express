043a62b8ae8289c3cf98a073444d9806eb9de5ae
saikittu332 <saikittu332@github.com>
2019-09-23 22:39:13 +0530

    sai

--- frontend/south-india-front/src/components/admin/managepackage.vue
+++ frontend/south-india-front/src/components/admin/managepackage.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="managetrain col-md-12">
+    <div class="col-md-12">
+      <h1>Manage Packages</h1>
+    </div>
     <hr>
     <div class="view">
         <table class="table">
@@ -7,47 +10,54 @@
             <thead class="thead-light">
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">Train No</th>
-                    <th scope="col">Train Name</th>
-                    <th scope="col">Package Id</th>
+                    <th scope="col">Pakage Id</th>
+                    <th scope="col">Package Name</th>
+                    <th scope="col">Source</th>
+                    <th scope="col">Destination</th>
+                    <th scope="col">Fare Id</th>
                     <th scope="col">Action</th>
                 </tr>
             </thead>
 
             <tbody>
-                <tr v-for="package in packages">
-                    <th scope="row">{{package.length}}</th>
-                    <td>{{train.train_number}}</td>
-                    <td>{{train.train_name}}</td>
-                    <td>{{train.package_id}}</td>
-                    <td><button v-on:click="deletetrain(train.train_number)">Delete</button></td>
+                <tr v-for="apackage in packages">
+                    <th scope="row">{{apackage.length}}</th>
+                    <td>{{apackage.package_id}}</td>
+                    <td>{{apackage.package_name}}</td>
+                    <td>{{apackage.source}}</td>
+                    <td>{{apackage.destination}}</td>
+                    <td>{{apackage.fare_id}}</td>
+                    <td><button v-on:click="deletepackage(apackage.package_id)">Delete</button></td>
                 </tr>
             </tbody>
         </table> 
     <div class="option">
-        <button v-on:click="addtrain=!addtrain">Add Trains</button>
+        <button v-on:click="addpackage=!addpackage">Add Packages</button>
     </div>        
     </div>
 
-    <div class="add col-md-12" v-if="addtrain">
+    <div class="add col-md-12" v-if="addpackage">
         
-            <label for="tno">Train No : </label>
-            <input type="text" v-model="train_number" placeholder="Train Number" >
-            <br>
-            <label for="tname">Train Name : </label>
-            <input type="text" v-model="train_name" placeholder="Train Name" >
-            <br>
             <label for="pid">Package Id : </label>
             <select v-model="package_id">
-                <option v-for="megad in mega">{{megad.package_id}}</option>
+                <option v-for="displayd in display" >{{displayd.package_id}}</option>
             </select>
             <br>
-            <label for="tid">Time Id : </label>
-            <select v-model="time_id">
-                <option v-for="megad in mega" >{{megad.time_id}}</option>
+            <label for="pname">Package Name : </label>
+            <input type="text" v-model="package_name" placeholder="Package Name" >
+            <br>
+            <label for="src">Source : </label>
+            <input type="text" v-model="source" placeholder="Source Stattion" >
+            <br>
+            <label for="dest">Destination : </label>
+            <input type="text" v-model="destination" placeholder="Destination Stattion" >
+            <br>
+            <label for="fid">Fare Id : </label>
+            <select v-model="fare_id">
+                <option v-for="displayd in display" >{{displayd.fare_id}}</option>
             </select>
             <br>  
-            <button v-on:click="addatrain">Add</button>
+            <button v-on:click="addapackage">Add</button>
     </div>  
  
   </div>
@@ -55,20 +65,21 @@
 
 <script>
 export default {
-  name: 'managetrain',
+  name: 'managepackage',
   props: {
   },
   data(){
     return {
-      viewtrain:true,
-      addtrain:false,
+      viewpackage:true,
+      addpackage:false,
       packages:[],
-      mega:[],
+      display:[],
       index:1,
-      train_number:null,
-      train_name:null,
       package_id:null,
-      time_id:null,
+      package_name:null,
+      source:null,
+      destination:null,
+      fare_id:null,
       delete:[],
       tr:[]
     }
@@ -80,36 +91,37 @@ export default {
         console.log(this.packages)
       })
 
-    //    this.$http.get('http://localhost:8080/api/display/mega')
-    //   .then(response=>{
-    //     this.mega= response.body;
-    //   })
+       this.$http.get('http://localhost:8080/api/display')
+      .then(response=>{
+        this.display= response.body;
+      })
   },
   methods:{
-    //   deletetrain:function(id){
-    //      this.$http.delete('http://localhost:8080/api/trains/' + id)
-    //     .then(response=>{
-    //     this.delete= response.body;
-    //             this.$swal('Train with id:'+ id +' has been deleted please refresh')
+      deletepackage:function(id){
+         this.$http.delete('http://localhost:8080/api/packages/' + id)
+        .then(response=>{
+        this.delete= response.body;
+                this.$swal('Package with id:'+ id +' has been deleted please refresh')
 
-    //     console.log(this.delete)
-    //   })
+        console.log(this.delete)
+      })
 
-    //   },
-    //   addatrain:function(){
-    //      this.$http.post('http://localhost:8080/api/package/', {
-    //          train_number:this.train_number,
-    //          train_name:this.train_name,
-    //          package_id:this.package_id,
-    //          time_id:this.time_id
-    //      })
-    //     .then(response=>{
-    //     this.tr= response.body;
-    //     this.$swal('Train has been added please refresh')
-    //     console.log(this.tr)
-    //   })
+      },
+      addapackage:function(){
+         this.$http.post('http://localhost:8080/api/packages/', {
+             package_id:this.package_id,
+             package_name:this.package_name,
+             source:this.source,
+             destination:this.destination,
+             fare_id:this.fare_id
+         })
+        .then(response=>{
+        this.tr= response.body;
+        this.$swal('Package has been added please refresh')
+        console.log(this.tr)
+      })
 
-    //   }
+      }
   }
 }
 </script>
